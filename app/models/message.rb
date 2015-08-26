ec8879c460e6c7200d213e0dca41c4a7e7d3f4eb
KeiTamiya <webcreatorhouse@gmail.com>
2015-08-26 07:37:56 +0000

    challege2_10

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     # enter the name definitely
     validates :name , length: { maximum: 20 } , presence: true
-    # enter the contents definitely and the num is 2 to 30
+    # enter the age in number definitely and the num must be 0 to 1000
+    validates :age , :inclusion => { :in => 0..1000, :message => "must be between 0 and 1000" } , presence: true
+    # enter the contents definitely and the num must be 2 to 30
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
 end
