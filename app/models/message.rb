08b500ffedaff482a640fd36009abbc2236a36a2
KeiTamiya <webcreatorhouse@gmail.com>
2015-08-26 02:56:29 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    # enter the name definitely
+    validates :name , length: { maximum: 20 } , presence: true
+    # enter the contents definitely and the num is 2 to 30
+    validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
 end
