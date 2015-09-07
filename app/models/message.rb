518ac2db0bce0e8b14b7f3fe7c7dda7a316b6a7c
KeiTamiya <webcreatorhouse@gmail.com>
2015-09-07 11:35:34 +0000

    add validates that age must be integer

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
     # enter the name definitely
     validates :name , length: { maximum: 20 } , presence: true
     # enter the age in number definitely and the num must be 0 to 1000
-    validates :age , :inclusion => { :in => 0..1000, :message => "must be between 0 and 1000" } , presence: true
+    validates :age , :inclusion => { :in => 0..1000, :message => "must be between 0 and 1000" } , numericality: { only_integer: true } , presence: true
     # enter the contents definitely and the num must be 2 to 30
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
 end
